bef551305d3f3b087a83678bba556012fec863e1
tamzid620 <tamzid620@gmail.com>
2025-07-04 11:14:48 +0600

    origin port changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 const { ObjectId } = require("mongodb");
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173/", "https://back-end-task-xi.vercel.app/"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true
 }));
